ea96ef67c28f95fa2bbdb858e1de32b10e4d9fd7
wssysh <1179855517@qq.com>
2018-06-12 22:13:36 +0800

    modify

--- List0301.c
+++ List0301.c
@@ -1,8 +1,26 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include "io.h"
+#include <math.h>
+#include <time.h>
+
+#define OK 1
+#define ERROR 0
+#define TRUE 1
+#define FALSE 0
 
 /*线性表的顺序储存结构类型描述*/
 #define MAXSIZE 20 //储存空间初始分配量
-typedef int ElemType; //假设元素类型为 int
+
+typedef int ElemType;	 //假设元素类型为 int
+typedef int Status       //Status函数类型，其值为状态代码eg：OK
+
+Status visit(ElemType e)
+{
+    printf("%d\n",e);
+    return OK;
+}
+
 typedef struct
 {
     ElemType data[MAXSIZE];
@@ -20,37 +38,75 @@ typedef struct
 //L.data=(ElemType*)malloc(sizeof(ElemType)*INITSIZE;
 //L.data=new ElemType[INITSIZE];
 
+Status InitList(SqList *L)//初始化顺序线性表
+{
+    L->length = 0;
+    return OK;
+}
+
+Status ListEmpty(SqList L)  //判空操作  初始条件：顺序线性表已存在
+{
+    if(L.length==0)
+        return TRUE;
+    else
+        return FALSE;
+}
+
+Status ClearList(SqList *L) //重置操作  初始条件：顺序线性表已存在
+{
+    L->length=0;
+    return OK;
+}
+
+int ListLength(SqList L)
+{
+    return L.length;
+}
+
 /*获取元素操作
   初始条件顺序线性表L存在1<=i<=ListLength(L)
-  GetElem(L, i, ElemType &e) //用e获取第i位的元素的值*/
-#define OK 1
-#define ERROR 0
-#define TURE 0
-#define FALSE 0
-typedef int Status      //Status函数类型，其值为状态代码eg：OK
-Status GetElem(SqList, int i, ElemType *e)
+  GetElem(L, i, &e) //用e获取第i位的元素的值*/
+Status GetElem(SqList L, int i, ElemType *e)
 {
     if(L.length == 0||i<1||i>L.length)
         return ERROR;
     *e = L.data[i-1];
     return OK;
 }
+/* 初始条件：顺序线性表L已存在 */
+/* 操作结果：返回L中第1个与e满足关系的数据元素的位序。 
+   若这样的数据元素不存在，则返回值为0 */
+int LocateElem(L, e)
+{
+    int i;
+    if(L.length==0)
+        return 0;
+    for(i=0;i<l.length;i++)
+    {
+        if(L.data[i]==e)
+            break;
+    }
+    if(i>=L.Length)
+        return 0;
+    return i+1;
+}
+
 /*ListInsert(&L, i, e) //插入操作
 初始条件：顺序线性表L已存在，1<=i<=ListLength(L)
 操作结果：在L中第i个位置之前插入新的数据元素e，L的长度加1*/
 Status ListInsert(SqList *L, int i, ElemType e)
 {
     int k;
-    if(L.length==MAXSIZE)
+    if(L->length==MAXSIZE)
         return ERROR;       //线性表已满
-    if(i<1||i>L.length+1)
+    if(i<1||i>L->length+1)
         return ERROR;       //i不在范围内
     if(i<=L->length)
     {
         for(k=L->length-1;k>=i-1;k--)
-            L->data[k+1]=L.data[k];     //插入数据不在表尾
+            L->data[k+1]=L->data[k];     //插入数据不在表尾
     }
-    L.data[i-1]=e;
+    L->data[i-1]=e;
     L->length++;
     return OK;
 }
@@ -61,18 +117,18 @@ Status ListInsert(SqList *L, int i, ElemType e)
 Status ListDelete(SqList *L, int i, ElemType *e)
 {
     int k;
-    if(L.length==0)
+    if(L->length==0)
         return ERROR;   //线性表为空
-    if(i<1||i>L.length)
+    if(i<1||i>L->length)
         return ERROR;   //删除位置不正确
-    *e = L.data[i-1];
-    if(i<L.length)
+    *e = L->data[i-1];
+    if(i<L->length)
     {
-        for(k=i;k<L.length;k++)
+        for(k=i;k<L->length;k++)
         {
-            L.data[k-1]=L.data[k];
+            L->data[k-1]=L->data[k];
         }
-        L.length--;
+        L->length--;
         return OK;
     }
 }
