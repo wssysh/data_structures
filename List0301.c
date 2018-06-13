09f919dd2bfe7bc99d6c603b3ff85590c35f727b
wssysh <1179855517@qq.com>
2018-06-13 17:25:34 +0800

    0.1 complete

--- List0301.c
+++ List0301.c
@@ -1,8 +1,8 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include "io.h"
-#include <math.h>
-#include <time.h>
+#include "stdio.h"
+//#include <stdlib.h>
+//#include "io.h"
+//#include <math.h>
+//#include <time.h>
 
 #define OK 1
 #define ERROR 0
@@ -17,7 +17,7 @@ typedef int Status       //Status函数类型，其值为状态代码eg：OK
 
 Status visit(ElemType e)
 {
-    printf("%d\n",e);
+    printf("%d ",e);
     return OK;
 }
 
@@ -37,6 +37,92 @@ typedef struct
 */
 //L.data=(ElemType*)malloc(sizeof(ElemType)*INITSIZE;
 //L.data=new ElemType[INITSIZE];
+
+/******************函数声明*******************/
+Status InitList(SqList *L);//初始化顺序线性表
+Status ListEmpty(SqList L);  //判空操作  初始条件：顺序线性表已存在
+Status ClearList(SqList *L); //重置操作  初始条件：顺序线性表已存在
+int ListLength(SqList L);   //取length
+Status GetElem(SqList L, int i, ElemType *e);//获取第i个元素
+int LocateElem(L, e);      //找到元素e在L中的位置
+Status ListInsert(SqList *L, int i, ElemType e);//第i个位置之前插入操作
+Status ListDelete(SqList *L, int i, ElemType *e);//元素删除
+Status ListTraverse(SqList L);       //对L的每个数据输出
+void UnionL(SqList *La, SqList Lb);  //合并操作
+
+int main()              //实际运用
+{
+    SqList L, Lb,;
+    ElemType e;     //temp，用来存放取得的数据元素
+    Status sta;     //状态判断
+    int j,k;        //循环变量 和 位置变量
+    sta=InitList(&L);     //初始化L链表
+    printf("初始化L后：L.length=%d\n",L.length);
+
+    //接下来两拨骚操作
+    for(j=1;j<=5;j++)
+        sta=ListInsert(&L,1,j);
+    printf("在L的表头依次插入1~5后：L.data=");
+    ListTraverse(L);
+    printf("L.length=%d \n",L.length);
+    sta=ListEmpty(L);
+    printf("L是否空：status=%d(1:是 0:否)\n",sta);
+
+    sta=ClearList(&L);
+    printf("L是否空：status=%d(1:是 0:否)\n",sta);
+    for(j=1;j<=10;j++)
+        sta=ListInsert(&L,j,j);
+    printf("在L的表尾依次插入1~10后：L.data=");
+    ListTraverse(L);
+    printf("L.length=%d \n",L.length);
+
+    GetElem(L,5,&e);
+    printf("第5个元素的值为：%d\n",e);
+    for(j=6;j<=7;j++)
+    {
+        k=LocateElem(L,j);
+        if(k)
+            printf("第%d个元素的值为%d\n",k,j);
+        else
+            printf("没有值为%d的元素\n",j);
+    }
+    k=ListLength(L);        //表的长度
+    
+    //删除操作
+    for(j=k-5;j>=k-4;j--)
+    {
+        sta=ListDelete(&L,j,&e);    //删除了第j个数据
+        if(sta)
+        {
+            printf("删除的第%d个元素为%d\n",j,e);
+        }
+        else
+            printf("Fail to delete the %dth data.\n",j);
+    }
+
+    printf("Print the datas one by one:");
+    ListTraverse(L);
+
+    j=5;
+    ListDelete(&L,j,&e);    //删除第五个数据
+    printf("第%d个元素的值为%d\n",j，e);
+
+    printf("Print the datas one by one:");
+    ListTraverse(L);
+
+    sta=InitList(&Lb);
+
+    for(j=1;j<=5;j++)
+        sta=ListInsert(&L,j,j*17);
+
+    UnionL(&L,Lb);
+    printf("Print the datas one by one:");
+    ListTraverse(L);
+
+    return 0;
+
+}
+
 
 Status InitList(SqList *L)//初始化顺序线性表
 {
@@ -133,9 +219,27 @@ Status ListDelete(SqList *L, int i, ElemType *e)
     }
 }
 
+Status ListTraverse(SqList L)       //对L的每个数据输出
+{
+    int i;
+    for(i=0;i<L.length;i++)
+    {
+        visit(L.data[i]);
+    }
+    printf("\n");
+    return OK;
+}
 
-
-
-
-
-
+void UnionL(SqList *La, SqList Lb)  //把Lb中和La不同的元素插入到La中
+{
+    int La_len, Lb_len, i;
+    ElemType e;
+    La_len=ListLength(*La);
+    Lb_len=ListLength(Lb);
+    for(i=1;i<=Lb_len;i++)
+    {
+        GetElem(Lb,i,&e);
+        if(!LocateElem(*La,e))
+            ListInsert(La,++La_len,e);
+    }
+}
